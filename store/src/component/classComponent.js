61ce0fd27861af9a5f3000ab8b67aed224b04f41
AnasBarakat2 <anasbarakat92@gmail.com>
2026-01-27 21:43:17 +0300

    update

---
+++ store/src/component/classComponent.js
@@ -0,0 +1,22 @@
+import React from "react"
+import '../css/nav.css'
+class ClassPage extends React.Component {
+
+    render(){
+        return(
+            <div id="nav">
+               <ul>
+                <li>Home</li>
+                  <li>contact</li>
+                    <li>aboutUs</li>
+               </ul>
+            </div>
+
+            
+        )
+
+        
+    }
+}
+
+export default ClassPage
