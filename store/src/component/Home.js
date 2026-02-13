3be820bf4cef5c0067a059fe071f1eca86b00990
AnasBarakat2 <anasbarakat92@gmail.com>
2026-02-13 19:39:50 +0300

    update

--- store/src/component/Home.js
+++ store/src/component/Home.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 function Home (){
+
+    const[ShowImg,SetShowImg]=useState(false) //هون انا عملت متغير اسمه showImg 
+                                               //setShow :بعمله عشان اغير حالة المتغير و قيمته
+    const handelClick=()=>{
+        SetShowImg(!ShowImg) //عملنا متغير على صيغة Function 
+                             //و طلبت منه تكون قيمته عكس لقيمة الshowImg و طلبت يخزنها بالSetShow
+    }
     return(
-        <div>
-             Home Page
+        <div> 
+            
+             <button onClick={handelClick}>{ShowImg?'hide':'show'}</button> 
+
+             {/*الكود بين الاقواس بالاسفل اسمه condition rendering
+             (Condition ? True : False)
+              */}
+
+             {
+                ShowImg && <img width={200} src="https://www.iloveimg.com/storage/blog/80-1638522537-Add-stars-and-clouds-to-sky-photography-online-using-a-free-photo-editor.jpg"></img>
+             } {/*هون بشوف قيمة الshow اذا true رح يعرض الصورة */}
         </div>
     )
 }
